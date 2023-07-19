ac9cad8d42e7490054e8eda1bfa1502974326c7e
Hiroaki Masuzawa <masuzawa.hiroaki.vl@tut.jp>
2023-07-19 13:25:28 +0900

    add xhost

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
- docker run --rm -it --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --name docker_ros_ur5e ros_ur5e bash
+xhost +
+docker run --rm -it --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --name docker_ros_ur5e ros_ur5e bash
